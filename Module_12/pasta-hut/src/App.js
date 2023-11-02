4c480114177f1c8151ec385af3f53544bcc9f629
Aaron Kilpatrick <kilpatrick.aaron92@gmail.com>
2023-11-03 09:08:12 +1100

    Implement Overlay with close button

--- Module_12/pasta-hut/src/App.js
+++ Module_12/pasta-hut/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-
 // import Card from './components/UI/Card';
 import Banner from './components/Banner/Banner';
+import Overlay from './components/UI/Overlay';
 import Menu from './components/Menu/Menu';
 import Header from './Layouts/Header/Header';
 import Footer from './Layouts/Footer/Footer';
@@ -19,7 +19,8 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <React.Fragment nt>
+      {isCartOpen && <Overlay onCloseCart={closeCartHandler} />}
       <Header onOpenCart={openCartHandler} />
       <Banner />
 
